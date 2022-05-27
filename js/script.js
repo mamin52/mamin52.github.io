1f7eb0b86c5350db8c88165bfcf30b4244474443
mamin52 <106405656+mamin52@users.noreply.github.com>
2022-05-28 00:19:31 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox.png') {
+      myImage.setAttribute ('src','images/cat.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox.png');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Введите свое имя');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Добро пожаловать, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Добро пожаловать, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
